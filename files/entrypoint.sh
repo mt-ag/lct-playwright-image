39dab4ad920498ec350f4c61c39c32a9702ccdab
phartenfeller <pharti@hotmail.de>
2022-07-19 11:01:48 +0200

    copy traces to output folder

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -4,8 +4,16 @@ cp -r /app/volume/* /app/workdir/
 mkdir /app/volume/output
 cp /app/workdir/results.xml /app/volume/output/
 cp /app/workdir/output.log /app/volume/output/
+
+## move screenshots
 if [[ -d /app/workdir/screenshots/ ]]
 then
    mv /app/workdir/screenshots/ /app/volume/output/
+fi
+
+## move traces
+if [[ -d /app/workdir/test-results/ ]]
+then
+   mv /app/workdir/test-results/ /app/volume/output/traces
 fi
 
