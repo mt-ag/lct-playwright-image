036a6223776342107d50cc9fe4a4fe265f72a45b
phartenfeller <pharti@hotmail.de>
2022-09-19 15:27:20 +0200

    only create output folder if it does not exist yet

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp -r /app/volume/* /app/workdir/
 /app/workdir/run.sh | tee output.log
-mkdir /app/volume/output
+mkdir -p /app/volume/output
 cp /app/workdir/results.xml /app/volume/output/
 cp /app/workdir/output.log /app/volume/output/
 
