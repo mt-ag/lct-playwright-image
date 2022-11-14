509e3e79ad3a855d3db0f210737af5bef54fd21c
phartenfeller <pharti@hotmail.de>
2022-11-14 09:38:53 +0100

    check folder empty before move

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,8 +2,8 @@
 cp -r /app/volume/* /app/workdir/
 /app/workdir/run.sh | tee output.log
 mkdir -p /app/volume/output
-cp /app/workdir/results.xml /app/volume/output/
-cp /app/workdir/output.log /app/volume/output/
+cp /app/workdir/results.xml /app/volume/output/ || true
+cp /app/workdir/output.log /app/volume/output/ || true
 
 mkdir -p /app/volume/output/screenshots/
 mkdir -p /app/volume/output/traces/
@@ -11,13 +11,13 @@ mkdir -p /app/volume/output/traces/
 ## move screenshots
 if [[ -d /app/workdir/screenshots/ ]]
 then
-  mv /app/workdir/screenshots/* /app/volume/output/screenshots/
+  [ "$(ls -A /app/workdir/screenshots)" ] && mv /app/workdir/screenshots/* /app/volume/output/screenshots/
 fi
 
 ## move traces
 if [[ -d /app/workdir/test-results/ ]]
 then
-  mv /app/workdir/test-results/* /app/volume/output/traces/
+  [ "$(ls -A /app/workdir/test-results)" ] && mv /app/workdir/test-results/* /app/volume/output/traces/
 fi
 
 chmod -R 777 /app/volume/output
