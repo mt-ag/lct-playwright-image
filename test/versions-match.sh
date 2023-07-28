96ef191b71db18ab3a9c47a1ce5940cabeef2978
phartenfeller <pharti@hotmail.de>
2023-07-28 10:03:54 +0200

    als upgrade package.json version + added test

---
+++ test/versions-match.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Path to your Dockerfile and package.json
+DOCKERFILE_PATH="./Dockerfile"
+PACKAGE_JSON_PATH="./files/package.json"
+
+# Extract version from Dockerfile
+DOCKERFILE_VERSION=$(grep '^FROM' $DOCKERFILE_PATH | awk -F':' '{print $2}' | sed 's/-focal//' | sed 's/v//')
+
+# Extract version from package.json
+PACKAGE_JSON_VERSION=$(awk -F'"' '/@playwright\/test/ {print $4}' $PACKAGE_JSON_PATH)
+
+# Compare versions
+if [ "$DOCKERFILE_VERSION" = "$PACKAGE_JSON_VERSION" ]; then
+    echo "Versions match: $DOCKERFILE_VERSION"
+else
+    echo "Versions do not match. Dockerfile version: $DOCKERFILE_VERSION, package.json version: $PACKAGE_JSON_VERSION"
+fi
