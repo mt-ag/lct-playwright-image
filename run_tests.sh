5820b2a089995ce18aec7e839e6be20af48c1345
ItIsFabian <fa.al.neu@gmail.com>
2025-04-15 15:39:29 +0200

    added use of absolute paths

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 EXECUTION_TYPE=$1
 IMAGE_NAME="lct-playwright-image"
+CURRENT_DIR=$(pwd)
 
 if [ "$EXECUTION_TYPE" != "local" ] && [ "$EXECUTION_TYPE" != "latest" ]; then
   echo "argument must be either \"local\" or \"latest\"."
@@ -7,22 +8,22 @@ if [ "$EXECUTION_TYPE" != "local" ] && [ "$EXECUTION_TYPE" != "latest" ]; then
 fi
 
 function build_image_locally() {
-  docker build --no-cache . --file Dockerfile -t "${IMAGE_NAME}:${EXECUTION_TYPE}"
+  docker build . --file Dockerfile -t "${IMAGE_NAME}:${EXECUTION_TYPE}"
 }
 
 function prepare_test() {
-  rm -f ./test/results.xml
-  rm -f ./test/screenshots/example.png
+  rm -f "$CURRENT_DIR/test/results.xml"
+  rm -f "$CURRENT_DIR/test/screenshots/example.png"
 }
 
 function verify_initial_folderstructure_and_files() {
-  single_directory_exists  "./test/output"
-  single_directory_exists  "./test/screenshots"
-  single_directory_exists  "./test/traces"
-  single_directory_exists  "./test/tests"
-  single_file_exists       "./test/tests/test.spec.js"
-  single_file_exists       "./test/playwright.config.js"
-  single_file_exists       "./test/lctReporter.js"
+  single_directory_exists  "$CURRENT_DIR/test/output"
+  single_directory_exists  "$CURRENT_DIR/test/screenshots"
+  single_directory_exists  "$CURRENT_DIR/test/traces"
+  single_directory_exists  "$CURRENT_DIR/test/tests"
+  single_file_exists       "$CURRENT_DIR/test/tests/test.spec.js"
+  single_file_exists       "$CURRENT_DIR/test/playwright.config.js"
+  single_file_exists       "$CURRENT_DIR/test/lctReporter.js"
 }
 
 function single_file_exists() {
@@ -48,12 +49,12 @@ function single_directory_exists() {
 }
 
 function check_result() {
-  single_file_exists "./test/results.xml"
-  single_file_exists "./test/screenshots/example.png"
+  single_file_exists "$CURRENT_DIR/test/results.xml"
+  single_file_exists "$CURRENT_DIR/test/screenshots/example.png"
 }
 
 function run_container() {
-  docker run -u pwuser --rm --ipc=host -v ./test:/app/workdir "${IMAGE_NAME}:${EXECUTION_TYPE}"
+  docker run -u pwuser --rm --ipc=host -v "$CURRENT_DIR/test:/app/workdir" "${IMAGE_NAME}:${EXECUTION_TYPE}"
 }
 
 if [ "$EXECUTION_TYPE" = "local" ]; then
